bccff7c14901201a7ac2d5ebe2f95033c133b3be
Mauri <maurijunk02@gmail.com>
2022-11-03 22:17:00 -0300

    varios cambios de cart y cart details

--- models/cart.ts
+++ models/cart.ts
@@ -1,9 +1,9 @@
 import { Schema, model } from "mongoose";
 
 const cartSchema = new Schema({
-  name: {type:String, required:true},
-  price: {type:Number, required:true},
-  amount: {type:Number, required:true}
+  name: {type: String, required: true},
+  details: {type: Array, required: true},
+  total: {type:Number, required:true}
 });
 
 export default model("Carrito", cartSchema);
